74c5d4b9c79716ee6a48a0400742ae2c79b349d3
deekshikhaarun0604 <deekshiaarun2007@gmail.com>
2026-07-23 20:32:48 +0530

    Add running sum function for 1D array

---
+++ dsa+java/1480_Running_Sum_of_1D_Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int i;
+        int sum=0;
+        for( i=0;i<nums.length;i++){
+        sum=sum+nums[i];
+        nums[i]=sum;
+        }
+   return nums; }
+}
